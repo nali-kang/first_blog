d2138807fc56c910ab00eac9120fefec36a948ff
nali-kang <softnali92@gmail.com>
2024-09-03 21:50:26 +0900

    init

---
+++ config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+  name: "NaliBlog",
+  url: "https://example.com",
+  description: "Nali's next site",
+  author: "NaliCoding",
+  links: {
+    github: "https://github.com/nali-kang",
+  },
+};
+
+export type Siteconfig = typeof siteConfig;
