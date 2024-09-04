30eb07543e46e52b50f0eaa84a413bfac519d615
nali-kang <softnali92@gmail.com>
2024-09-04 12:58:46 +0900

    Blog 페이지 추가
    blog page, post-item 컴포넌트 추가, date KR,

---
+++ app/blog/page.tsx
@@ -0,0 +1,41 @@
+import { posts } from "#site/content";
+import { PostItem } from "@/components/post-item";
+import { sortPosts } from "@/lib/utils";
+
+export default async function BlogPage() {
+  const sortedPosts = sortPosts(posts.filter((post) => post.published));
+  const displayPosts = sortedPosts;
+
+  return (
+    <div className="container max-w-4xl py-6 lg:py-10">
+      <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8 ">
+        <div className="flex-1 space-y-4">
+          <h1 className="inline-block font-black text-4xl lg:text-5xl">Blog</h1>
+          <p className="text-xl text-muted-foreground">
+            어쩌고 저쩌고 내 블로그다
+          </p>
+        </div>
+      </div>
+      <hr className="mt-8" />
+      {displayPosts?.length > 0 ? (
+        <ul className="flex felx-col">
+          {displayPosts.map((post) => {
+            const { slug, date, title, description } = post;
+            return (
+              <li key={slug} className="w-full">
+                <PostItem
+                  slug={slug}
+                  date={date}
+                  title={title}
+                  description={description}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <p> 아무것도 없어유 </p>
+      )}
+    </div>
+  );
+}
